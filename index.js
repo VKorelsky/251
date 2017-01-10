07140715d7bb6f9fe36a6fa5ad31daca06a0f7a8
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-01-09 21:15:12 -0500

    layout and static files

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
-var num = 0
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.use('/static', express.static('assets'))
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+  response.render('home', {
+    name: 'world'
+  })
 })
 
-app.listen(port, console.log("Listening on port 3000"))
-
-// error handling middleware
+// error logging middleware
 app.use((err, request, response, next) => {
   console.log(err)
   response.status(500).send('Something broke!')
 })
+
+app.listen(port, console.log(`server is listening on ${port}`))
