d1e68090a3dbd1a1c9c3dce8853edde6cc49d5d3
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-01-10 12:04:30 -0500

    functioning seed

--- db/config.js
+++ db/config.js
@@ -1,6 +1,11 @@
-// require('seed-mongoose')();
+var seed = require('seed-mongoose')({
+            path : 'db/seeds'
+        });
+
 var mongoose = require('mongoose'),
     path = require('path');
+
+require(path.join(__dirname, '/..', 'models/Posts/Posts.js'));
 
 // Database
 mongoose.connect('mongodb://localhost/test');
@@ -10,5 +15,4 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function() {
   console.log("DB connection established");
-  require(path.join(__dirname, '/..', 'models/Posts/Posts.js'));
 });
