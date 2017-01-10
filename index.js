27f7db4e717253e751a8374b7007379ea0e65d02
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-01-09 20:33:10 -0500

    router implementation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-require('./app/index');
+const express = require('express')
+const app = express()
+const port = 3000
+
+var num = 0
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
+
+app.listen(port, console.log("Listening on port 3000"))
+
+// error handling middleware
+app.use((err, request, response, next) => {
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
