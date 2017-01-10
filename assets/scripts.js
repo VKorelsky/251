050427b0db5bd0010eadd8e4ff037381ffcfee14
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-01-10 15:48:15 -0500

    progress

--- assets/scripts.js
+++ assets/scripts.js
@@ -17,20 +17,37 @@ function submitPost(url, data) {
     url: url,
     data: data,
     type: 'POST',
-    success: function(response) { success(response);},
+    success: function(response) { success(response, data);},
     error: function(error) { console.log(err)}
   });
 };
 
-function success(response){
-  console.log("Success");
+function success(response, data){
   $('#notification').show();
 
   setTimeout(function(){
     $('#notification').hide();
   }, 2000);
 
+  renderItem(data)
 };
+
+function renderItem(data){
+  console.log(data);
+  var div = document.createElement('div');
+
+  div.innerHtml = '<div class="list-upvotes">' +
+              '<i class="fa fa-caret-up" aria-hidden="true"></i>' +
+              '<p>0</p>' +
+          '</div>' +
+          '<div class="list-main">' +
+              '<h4 class="title">'+ data.title + '</h4>' +
+              '<p class="body">'+ data.body + '</p>' +
+          '</div>';
+
+  console.log(div);
+  $('#item-container').append(div);
+}
 
 
 });
