2a5a93e1525a5486d16afa0e0a124cea86671d6e
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-01-09 22:48:43 -0500

    page styling

--- index.js
+++ index.js
@@ -13,12 +13,23 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
-app.use('/static', express.static('assets'))
+app.use(express.static(__dirname + '/assets'));
 
+// Routes
 app.get('/', (request, response) => {
+  //Fetch data in database
+
   response.render('home', {
     name: 'world'
   })
+})
+
+app.post('/', (request, response) => {
+  // Save post to DB
+  //return a json - 200 OK, 500 ERROR
+
+
+  response.render()
 })
 
 // error logging middleware
@@ -27,4 +38,6 @@ app.use((err, request, response, next) => {
   response.status(500).send('Something broke!')
 })
 
+
+// Run
 app.listen(port, console.log(`server is listening on ${port}`))
